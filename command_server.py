e3f907beea66a48bc41a9d2f6d675e3f322501a0
cbrwx <81207429+cbrwx@users.noreply.github.com>
2024-03-11 02:51:21 +0100

    Update command_server.py

--- command_server.py
+++ command_server.py
@@ -1,13 +1,22 @@
-# servercode for shell commands
 from flask import Flask, request, jsonify
 import subprocess
+
+# Define a secret key for authentication
+SECRET_KEY = "cbrwx"
 
 app = Flask(__name__)
 
 @app.route('/execute', methods=['POST'])
 def execute_command():
     data = request.json
-    command = data['command']
+    
+    # Extract the command and the provided secret key from the request
+    command = data.get('command')
+    provided_secret_key = data.get('secret_key')
+    
+    # Check if the provided secret key matches the expected secret key
+    if provided_secret_key != SECRET_KEY:
+        return jsonify({'error': 'Unauthorized access attempt. The secret key is invalid.'}), 401
 
     try:
         output = subprocess.check_output(command, shell=True, text=True, stderr=subprocess.STDOUT)
